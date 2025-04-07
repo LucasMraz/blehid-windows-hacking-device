7daf87881535855e5a7c82b3cbaa03fbb7b53385
Miso <154011067+LucasMraz@users.noreply.github.com>
2025-04-06 21:14:18 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,35 @@
+bluetooth.onBluetoothConnected(function () {
+    Bluetooth = true
+    while (Bluetooth == true) {
+        basic.showIcon(IconNames.Yes)
+    }
+})
+bluetooth.onBluetoothDisconnected(function () {
+    Bluetooth = false
+    while (Bluetooth == false) {
+        basic.showIcon(IconNames.SmallDiamond)
+        basic.showIcon(IconNames.Diamond)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    keyboard.sendString("" + keyboard.modifiers(keyboard._Modifier.windows) + "r")
+    keyboard.sendString("cmd")
+    keyboard.sendString(keyboard.keys(keyboard._Key.enter))
+    keyboard.sendString("You have been hacked")
+})
+input.onButtonPressed(Button.AB, function () {
+    keyboard.sendString("" + keyboard.modifiers(keyboard._Modifier.windows) + "x")
+    basic.pause(200)
+    keyboard.sendString("uu")
+})
+input.onButtonPressed(Button.B, function () {
+    keyboard.sendString("" + keyboard.modifiers(keyboard._Modifier.windows) + "x")
+    basic.pause(200)
+    keyboard.sendString("us")
+})
+let Bluetooth = false
+keyboard.startKeyboardService()
+while (Bluetooth == false) {
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Diamond)
+}
